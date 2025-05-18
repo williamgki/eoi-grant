097f22f839541f15635486f6858543eeaf04aa42
williamgki <williamgkirby@gmail.com>
2025-05-18 16:37:35 +0100

    chore: remove unused log export variable

--- infra/logs.tf
+++ infra/logs.tf
@@ -1,8 +1,3 @@
-variable "log_export_bucket" {
-  description = "Bucket for CloudWatch log exports"
-  type        = string
-}
-
 resource "aws_cloudwatch_log_group" "eoi_scorer" {
   name              = "/ecs/eoi-scorer"
   retention_in_days = 90
